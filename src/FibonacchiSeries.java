375433811c263c9c297422554578194ada450a1a
Safayet-Shafi <77004144+Safayet-Shafi@users.noreply.github.com>
2024-04-08 02:18:44 +0600

    fibonacchi

--- src/FibonacchiSeries.java
+++ src/FibonacchiSeries.java
@@ -1,2 +1,12 @@
 public class FibonacchiSeries {
+    void printFiboNacchi(int count){
+        int n1=0,n2=1,n3;
+        for(int i =0;i<count;i++){
+            System.out.print(n1 + " ");
+            n3=n1+n2;
+            n1=n2;
+            n2=n3;
+
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,7 @@ public class Main {
         Palindrome obj1= new Palindrome();
 //        System.out.println(obj1.palindromeString("ABAS"));
 //        System.out.println(obj1.palindromeNumber(456));
+        FibonacchiSeries obj2= new FibonacchiSeries();
+        obj2.printFiboNacchi(10);
     }
 }
